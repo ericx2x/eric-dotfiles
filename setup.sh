dfe20ea1939a21ba4c991a2a35e4bf5795f634a5
ericx2x <ericjlima@gmail.com>
2025-06-29 21:46:53 +0800

    added silver surfer and fzf

--- setup.sh
+++ setup.sh
@@ -15,6 +15,31 @@ install_zsh_autosuggestions() {
     git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 }
 
+# Function to install fzf
+install_fzf() {
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
+
+# Function to install The Silver Searcher
+install_silver_searcher() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if [ -x "$(command -v apt)" ]; then
+            sudo apt install -y silversearcher-ag
+        elif [ -x "$(command -v dnf)" ]; then
+            sudo dnf install -y the_silver_searcher
+        elif [ -x "$(command -v pacman)" ]; then
+            sudo pacman -S the_silver_searcher
+        else
+            echo "Unsupported Linux distribution. Please install The Silver Searcher manually."
+        fi
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install the_silver_searcher
+    else
+        echo "Unsupported OS. Please install The Silver Searcher manually."
+    fi
+}
+
 # Function to set up Zsh as the default shell
 set_default_shell() {
     chsh -s $(which zsh)
@@ -23,7 +48,6 @@ set_default_shell() {
 # Function to install Neovim
 install_neovim() {
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Linux
         if [ -x "$(command -v apt)" ]; then
             sudo add-apt-repository ppa:neovim-ppa/stable && sudo apt update && sudo apt install -y neovim
         elif [ -x "$(command -v dnf)" ]; then
@@ -34,10 +58,8 @@ install_neovim() {
             echo "Unsupported Linux distribution. Please install Neovim manually."
         fi
     elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # macOS
         brew install neovim
     elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
-        # Windows (using WSL or Cygwin)
         echo "Please install Neovim manually on Windows."
     else
         echo "Unsupported OS. Please install the required software manually."
@@ -53,19 +75,10 @@ install_powerlevel9k
 # Install zsh-autosuggestions
 install_zsh_autosuggestions
 
-# Symlink .zshrc
-ln -sf $(pwd)/.zshrc $HOME/.zshrc
+# Install fzf
+install_fzf
 
-# Create Neovim config directory if it doesn't exist
-mkdir -p $HOME/.config/nvim
+# Install The Silver Searcher
+install_silver_searcher
 
-# Symlink Neovim config
-ln -sf $(pwd)/nvim/init.vim $HOME/.config/nvim/init.vim
-
-# Install Neovim
-install_neovim
-
-# Change default shell to Zsh
-set_default_shell
-
-echo "Setup complete! Please restart your terminal."
+# Sy
